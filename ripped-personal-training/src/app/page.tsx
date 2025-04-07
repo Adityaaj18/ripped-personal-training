c6eee95cab0e27eb71440235bd74b384e7dbfea9
aditya jadhav <jadhavaditya033@gmail.com>
2025-04-07 12:02:29 +0100

    modifying homepage and added ui library called shadcn

---
+++ ripped-personal-training/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">Homee</h1>
+      <Button>Click</Button>
+    </div>
+  );
+}
