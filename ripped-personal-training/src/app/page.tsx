96c0e60583db83d593273e2d8b81e2b1a68736ad
aditya jadhav <jadhavaditya033@gmail.com>
2025-04-07 17:54:04 +0100

    modified homepage

--- ripped-personal-training/src/app/page.tsx
+++ ripped-personal-training/src/app/page.tsx
@@ -1,12 +1,41 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/ui/forms/CustomerForm";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">Homee</h1>
-      <Button>Click</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* {todo: otp verification} */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full2.svg"
+            height={1000}
+            width={1000}
+            alt="ripped"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 Ripped
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img2.jpg"
+        height={1000}
+        width={1000}
+        alt="Ripped"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
