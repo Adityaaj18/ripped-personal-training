847d1946495d9017c7e65b5a5cf296f05d68f96a
aditya jadhav <jadhavaditya033@gmail.com>
2025-04-16 20:10:40 +0100

    implemented SMS feature for appointments using twilio

--- ripped-personal-training/lib/actions/appointment.actions.ts
+++ ripped-personal-training/lib/actions/appointment.actions.ts
@@ -6,12 +6,16 @@ import {
   DATABASE_ID,
   databases,
   ENDPOINT,
+  messaging,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import App from "next/app";
 import { Appointment } from "../../types/appwrite.types";
 import { revalidatePath } from "next/cache";
+import { Content } from "next/font/google";
+import { formatDateTime } from "../../lib/utils";
 
+//create
 export const createAppointment = async (
   appointment: CreateAppointmentParams
 ) => {
@@ -28,6 +32,8 @@ export const createAppointment = async (
     console.log(error);
   }
 };
+
+//read
 
 export const getAppointment = async (appointmentId: string) => {
   try {
@@ -81,9 +87,12 @@ export const getRecentAppoitnmentList = async () => {
   }
 };
 
+//update
+
 export const updateAppointment = async ({
   appointmentId,
   userId,
+  timeZone,
   appointment,
   type,
 }: UpdateAppointmentParams) => {
@@ -92,15 +101,61 @@ export const updateAppointment = async ({
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
       appointmentId,
-      appointment
+      appointment,
     );
     if (!updateAppointment) {
       throw new Error("Appointment not found");
     }
-    //SMS notification
+
+    console.log('sss', appointment);
+    
+
+    const smsMessage = `Hi, it's Ripped.
+    ${type === "schedule" ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!, timeZone).dateTime} with ${appointment.primaryTrainer}` : 
+      `We regret to inform you that your appointment has been cancelled. Reason: ${appointment.cancellationResult}`}
+    `;
+
+    await sendSMSNotification(userId, smsMessage)
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//delete
+// export const deleteAppointment = async ({
+//   appointmentId,
+// }: {
+//   appointmentId: string;
+// }) => {
+//   try {
+//     const deleted = await databases.deleteDocument(
+//       DATABASE_ID!,
+//       APPOINTMENT_COLLECTION_ID!,
+//       appointmentId
+//     );
+
+//     revalidatePath("/admin");
+//     return { success: true, message: "Appointment deleted", data: deleted };
+//   } catch (error) {
+//     console.log(error);
+//     return { success: false, message: "Failed to delete appointment", error };
+//   }
+// };
+
+//SMS function
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+
+    return parseStringify(message);
   } catch (error) {
     console.log(error);
   }
